afe546ff9404be034f3ca599a641b80199968445
Leonardo <leonardobeirout@gmail.com>
2018-06-26 19:20:28 -0300

    Primeira alteração php

---
+++ galeria.php
@@ -0,0 +1,47 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<title>Padang Surf</title>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css?family=Fjalla+One|Kavivanar|Pacifico" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<link rel="shortcut icon" href="favicon.ico">
+</head>
+
+
+
+<body>
+	<nav>
+		<a href="index.html" class="nova-font"> Padang Surf </a>
+		<ul>
+			<li><a href="index.html#portfolio"> PORTFOLIO</a></li>
+			<li><a href="index.html#servicos"> SERVIÇOS</a></li>
+			<li><a href="index.html#contato"> CONTATO </a></li>
+		</ul>	
+	</nav>
+	
+		<header>
+			<h1 class="nova-font">Galeria</h1>
+		</header>
+
+
+<selection id="galeria">
+<?php
+echo "Teste de texto"
+?>
+</selection>	
+
+
+
+
+
+
+
+
+<footer>
+	<p>FPIN - PADANG</p>
+</footer>
+</body>
+</html>
